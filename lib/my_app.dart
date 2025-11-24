53bf69ab4d9859a38c94dae7888e167a97904bf6
Mikołaj Ożga <mikolaj.ozga@student.pk.edu.pl>
2025-11-24 19:59:28 +0100

    feat(step-2) ChatScreen + UI

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ai_text_assistant/features/chat/chat_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp ({super.key});
